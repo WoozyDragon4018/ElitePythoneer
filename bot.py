da13f50ba7f3e55a26d0c42c7cee6b26950886fe
WoozyDragon4018 <58138055+WoozyDragon4018@users.noreply.github.com>
2019-12-01 12:54:04 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+from discord.utils import get
+
+def get_prefix(bot, msg):
+    prefixes = ['?']
+
+    return commands.when_mentioned_or(*prefixes)(bot, msg)
+
+bot=commands.Bot(case_insensitive=True,command_prefix=get_prefix)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print("{} has successfully booted and running!".format(bot.user.name))
+
+@bot.command(pass_context = True)
+async def help(ctx):
+    embed=discord.Embed(color=0xFFFF)
+    embed.set_author(name='PyBot Help')
+    embed.add_field(name='No help added!', value='Bot is refreshed', inline=False)
+    await ctx.send(embed=embed)
+
+
+
+bot.run(os.getenv('NjQ5NTIzMDEyNTIwOTAyNjcx.XeNmeg.ULtnbAjeqVkcl6Vtt7XausjNr_o'))
