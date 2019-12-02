599dfa535e99b7122f2d29bc2e03ce679cbbb85d
WoozyDragon4018 <58138055+WoozyDragon4018@users.noreply.github.com>
2019-12-02 18:58:43 +0530

    logs added

--- bot.py
+++ bot.py
@@ -80,14 +80,14 @@ async def d(ctx, numi, numii):
     await ctx.send(str(numi) + ' / ' + str(numii) + ' = ' + str(sum_value))
 
 @bot.command(pass_context = True)
-async def clear(ctx,amount:int=10):
+async def clear(ctx,amount):
     if ctx.author.guild_permissions.manage_messages:
         await ctx.channel.purge(limit=amount+1)
         await ctx.send(f"{amount} message has been deleted."if(int(amount)is 1)else(f"{amount} messages have been deleted."),delete_after=5)
         channel = bot.get_channel(650348478056235014)
         embed = discord.Embed(color=0xFFFFFF)
         embed.set_author(name='Mod Command Used!')
-        embed.add_field(name='Clear Command used', value=f'{ctx.author} has used `purge` command.')
+        embed.add_field(name='Clear Command used', value=f'{ctx.author.mention} has used `purge` command.')
         await channel.send(embed=embed)
 
 @clear.error
@@ -101,11 +101,17 @@ async def clear_error(ctx, error):
 @bot.command(pass_context=True)
 async def warn(ctx, user: discord.User, *, reason=None):
     await ctx.channel.purge(limit=1)
-    embed = discord.Embed(color=0xFFFF)
-    embed.set_author(name=f'Warning')
-    embed.add_field(name='You were warned in Elite Programmers group for :', value=reason, inline=False)
-    await user.send(embed=embed)
+    embeda = discord.Embed(color=0xFFFF)
+    embeda.set_author(name=f'Warning')
+    embeda.add_field(name='You were warned in Elite Programmers group for :', value=reason, inline=False)
+    await user.send(embed=embeda)
     await ctx.send(str(user) + ' has succesfully been warned for ' + reason)
+    channel = bot.get_channel(650348478056235014)
+    embed = discord.Embed(color=0xFFFFFF)
+    embed.set_author(name='Mod Command Used!')
+    embed.add_field(name='Warn Command used', value=f'{ctx.author.mention} has warned ' + str(user), inline=False)
+    embed.add_field(name='Reason : ', value=reason, inline=False)
+    await channel.send(embed=embed)
     
 @bot.event
 async def on_message(message):
@@ -114,22 +120,34 @@ async def on_message(message):
 @commands.has_role("Staff")
 @bot.command(pass_context=True)
 async def kick(ctx, user:discord.Member, *, reason=None):
-    embed = discord.Embed(color=0xFFFF)
-    embed.set_author(name='Kicked!')
-    embed.add_field(name='You were kicked from Elite Programmers Group for :', value=reason, inline=False)
-    await user.send(embed=embed)
+    embeda = discord.Embed(color=0xFFFF)
+    embeda.set_author(name='Kicked!')
+    embeda.add_field(name='You were kicked from Elite Programmers Group for :', value=reason, inline=False)
+    await user.send(embed=embeda)
     await user.kick(reason=reason)
     await ctx.send(str(user) + ' has succesfully been kicked for : ' + reason)
+    channel = bot.get_channel(650348478056235014)
+    embed = discord.Embed(color=0xFFFFFF)
+    embed.set_author(name='Mod Command Used!')
+    embed.add_field(name='Kick Command used', value=f'{ctx.author.mention} has kicked ' + str(user), inline=False)
+    embed.add_field(name='Reason : ', value=reason, inline=False)
+    await channel.send(embed=embed)
         
 @commands.has_role("Staff")
 @bot.command(pass_context=True)
 async def ban(ctx, user:discord.Member, *, reason=None):
-    embed = discord.Embed(color=0xFFFF)
-    embed.set_author(name='Kicked!')
-    embed.add_field(name='You were kicked from Elite Programmers Group for :', value=reason, inline=False)
-    await user.send(embed=embed)
+    embeda = discord.Embed(color=0xFFFF)
+    embeda.set_author(name='Kicked!')
+    embeda.add_field(name='You were kicked from Elite Programmers Group for :', value=reason, inline=False)
+    await user.send(embed=embeda)
     await user.ban(reason=reason)
     await ctx.send(str(user) + ' has succesfully been banned for : ' + reason)
+    channel = bot.get_channel(650348478056235014)
+    embed = discord.Embed(color=0xFFFFFF)
+    embed.set_author(name='Mod Command Used!')
+    embed.add_field(name='Ban Command used', value=f'{ctx.author.mention} has banned ' + str(user), inline=False)
+    embed.add_field(name='Reason : ', value=reason, inline=False)
+    await channel.send(embed=embed)
 
 #Note to Self : Use ff0000 as the hex for red color.
 #embed = discord.Embed(color=0xff0000)
